ff6228a22ccb6bda57cfa6067662986c5097a8b3
Kieran Bamforth <kieran@kierans-mbp.default>
2014-05-18 22:47:01 +0100

    something something something.

--- manifests/site.pp
+++ manifests/site.pp
@@ -67,8 +67,7 @@ node default {
   include nodejs::v0_10
 
   # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.1.0': }
+  # ruby::version { '2.1.0': }
 
   # common, useful packages
   package {
