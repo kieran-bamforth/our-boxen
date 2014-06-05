8c26f5a3b0a31281d4a5e70b952f2fa4fe8e8f29
Kieran Bamforth <kieranb@studioskylab.com>
2014-06-05 12:05:22 +0100

    Shows the hidden files.

--- manifests/site.pp
+++ manifests/site.pp
@@ -112,6 +112,7 @@ include osx::global::expand_save_dialog
 include osx::dock::clear_dock
 
 include osx::finder::unhide_library
+include osx::finder::show_hidden_files
 
 include osx::disable_app_quarantine
 include osx::software_update
