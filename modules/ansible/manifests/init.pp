29b75357231def45b94f708c2c92059d450d9fe0
Kieran Bamforth <kieranb@studioskylab.com>
2014-06-09 20:02:29 +0100

    updated ansible.

--- modules/ansible/manifests/init.pp
+++ modules/ansible/manifests/init.pp
@@ -3,14 +3,14 @@ class ansible {
       ensure => present,
     }
 
-    file { ['/etc/ansible', '/etc/ansible/hosts', ]:
+    file { ['/etc/ansible', '/etc/ansible/hosts', '/etc/ansible/plugins', '/etc/ansible/plugins/inventory']:
         ensure => 'directory',
         owner  => 'kieran',
         group  => 'staff',
         mode   => 755,
     }
 
-    file { '/etc/ansible/ec2.py':
+    file { '/etc/ansible/plugins/inventory/ec2.py':
         ensure => 'present',
         owner => 'kieran',
         group => 'staff',
