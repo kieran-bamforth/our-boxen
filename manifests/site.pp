7eec651d3fbd861e94d1863eb6877800b0ff56e6
Kieran Bamforth <hello@kieranbamforth.me>
2015-08-04 13:49:45 +0100

    here is intl / xdebug.

--- manifests/site.pp
+++ manifests/site.pp
@@ -116,8 +116,8 @@ node default {
 
   class { 'php::global': version => '5.6.9'}
   include php::composer
-  # php::extension::intl { 'intl for 5.6.9': php => '5.6.9'}
-  # php::extension::xdebug { 'xdebug for 5.6.9': php => '5.6.9'}
+  php::extension::intl { 'intl for 5.6.9': php => '5.6.9', version => '3.0.0'}
+  php::extension::xdebug { 'xdebug for 5.6.9': php => '5.6.9', version => '2.3.2' }
 
   include phpstorm
   include pkgconfig
