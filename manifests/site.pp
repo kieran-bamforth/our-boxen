16ee33dcf412007e5b1bb29696da588db2fa73c2
Kieran Bamforth <hello@kieranbamforth.me>
2015-11-04 23:07:11 +0000

    emmet and html prettify.

--- manifests/site.pp
+++ manifests/site.pp
@@ -149,10 +149,12 @@ node default {
   include sublime_text_3
   include sublime_text_3::package_control
   sublime_text_3::package { 'BracketHighlighter': source => 'facelessuser/BracketHighlighter' }
+  sublime_text_3::package { 'emmet-sublime': source => 'sergeche/emmet-sublime' }
+  sublime_text_3::package { 'Material Theme': source => 'equinusocio/material-theme' }
+  sublime_text_3::package { 'Sublime-HTMLPrettify': source => 'victorporof/Sublime-HTMLPrettify'}
   sublime_text_3::package { 'sublime-text-puppet': source => 'eklein/sublime-text-puppet' }
   sublime_text_3::package { 'TrailingSpaces': source => 'SublimeText/TrailingSpaces' }
-  sublime_text_3::package { 'Material Theme': source => 'equinusocio/material-theme' }
-  sublime_text_3::package { 'tern_for_sublime': source => 'ternjs/tern_for_sublime'}
+
 
   include tmux
   class { 'vagrant': completion => true }
