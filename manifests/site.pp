c822eec32bf22ac4f72491c31942afff45d8a35c
Kieran Bamforth <hello@kieranbamforth.me>
2015-12-07 21:41:25 +0000

    Added aws cli.

--- manifests/site.pp
+++ manifests/site.pp
@@ -138,6 +138,10 @@ node default {
   include pkgconfig
 
   python::version { '2.7.6': }
+  python::package { 'awscli for 2.7.6':
+    package => 'awscli',
+    python => '2.7.6'
+  }
   class { 'python::global': version => '2.7.6' }
 
   ruby::version { '2.0.0': }
---
+++ modules/autojump/init.pp
@@ -0,0 +1,5 @@
+class autojump {
+    package { 'autojump':
+      ensure => present
+    }
+}
