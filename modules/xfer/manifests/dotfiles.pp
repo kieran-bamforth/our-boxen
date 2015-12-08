75d1b38d00aa24d98629b70b889e372b0772dac6
Kieran Bamforth <hello@kieranbamforth.me>
2015-12-08 12:54:13 +0000

    The sublime settings are now coming from the dotfiles.

--- modules/xfer/manifests/dotfiles.pp
+++ modules/xfer/manifests/dotfiles.pp
@@ -1,12 +1,15 @@
 class xfer::dotfiles {
     $home = "/Users/${::boxen_user}"
     $dotfiles = "${home}/dotfiles"
+    $sublimefiles = "${home}/Library/Application Support/Sublime Text 3/Packages/User"
+    $dfsublime = "${sublimefiles}/Preferences.sublime-settings"
     $dftmux = "${home}/.tmux.conf"
     $dfvimrc = "${home}/.vimrc"
     $dfzsh = "${home}/.zshrc"
 
     file { $dotfiles:
-        ensure  => directory
+        ensure  => 'directory',
+        require => File[$sublimefiles]
     }
 
     repository { $dotfiles:
@@ -30,5 +33,10 @@ class xfer::dotfiles {
         target => "$dotfiles/.zshrc",
         ensure => 'link',
         require => Repository[$dotfiles]
+    }
+
+    file { $dfsublime:
+        target => "$dotfiles/Preferences.sublime-settings",
+        ensure => 'link',
     }
 }
