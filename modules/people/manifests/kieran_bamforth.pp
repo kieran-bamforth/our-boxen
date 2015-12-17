c4867d3505f43bba6ea7cdc2f2a2abd84e5b9054
Kieran Bamforth <hello@kieranbamforth.me>
2015-12-17 16:38:55 +0000

    Added flow project

--- modules/people/manifests/kieran_bamforth.pp
+++ modules/people/manifests/kieran_bamforth.pp
@@ -2,4 +2,5 @@
 class people::kieran_bamforth {
 	include projects::alcn
 	include projects::dotfiles
+	include projects::flow
 }
---
+++ modules/projects/manifests/flow.pp
@@ -0,0 +1,9 @@
+# Project Manifests
+
+class projects::flow {
+
+    boxen::project { 'flow':
+        source => 'kieran-bamforth/flow'
+    }
+    
+}
