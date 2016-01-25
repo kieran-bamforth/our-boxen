3a5e9d6c9a7b74e8a04702d71cab78b707487d61
Kieran Bamforth <hello@kieranbamforth.me>
2016-01-25 23:50:18 +0000

    wait you need these.

---
+++ modules/projects/manifests/aws.pp
@@ -0,0 +1,7 @@
+# Project Manifests
+
+class projects::alcn {
+	boxen::project { 'alcn':
+		source => 'https://kieran-bamforth@bitbucket.org/kieran-bamforth/aws.git'
+	}
+}
---
+++ modules/projects/manifests/keys.pp
@@ -0,0 +1,9 @@
+# Project Manifests
+
+class projects::keys {
+
+    boxen::project { 'keys':
+        source => 'https://kieran-bamforth@bitbucket.org/kieranbamforth/keys.git'
+    }
+    
+}
