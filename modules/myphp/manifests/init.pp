639ce74345ac5c6f9ca321058f31d3a70de34e48
Kieran Bamforth <hello@kieranbamforth.me>
2016-05-10 14:20:44 +0100

    imagemagick.

--- modules/myphp/manifests/init.pp
+++ modules/myphp/manifests/init.pp
@@ -3,4 +3,5 @@ class myphp {
 	include php::composer
 	php::extension::intl { 'intl for 5.6.9': php => '5.6.9', version => '3.0.0'}
 	php::extension::xdebug { 'xdebug for 5.6.9': php => '5.6.9', version => '2.3.2' }
+	php::extension::imagick { 'imagick for 5.6.9': php => '5.6.9', version => '3.0.0' } 
 }
