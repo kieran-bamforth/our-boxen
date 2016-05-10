2db8e21490300ee9a231fbf648d6a9ebc362974f
kieranbamforth <hello@kieranbamforth.me>
2016-05-10 21:00:31 +0100

    imageagick trying a thing.

--- modules/myphp/manifests/init.pp
+++ modules/myphp/manifests/init.pp
@@ -1,7 +1,7 @@
 class myphp {
 	class { 'php::global': version => '5.6.9'}
 	include php::composer
+	include imagemagick
 	php::extension::intl { 'intl for 5.6.9': php => '5.6.9', version => '3.0.0'}
 	php::extension::xdebug { 'xdebug for 5.6.9': php => '5.6.9', version => '2.3.2' }
-	php::extension::imagick { 'imagick for 5.6.9': php => '5.6.9', version => '3.0.0' } 
 }
