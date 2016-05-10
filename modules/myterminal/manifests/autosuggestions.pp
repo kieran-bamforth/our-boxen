4e635778a124ff7b594fe764da857924730055ed
kieranbamforth <hello@kieranbamforth.me>
2016-05-10 23:02:59 +0100

    updating the autosuggestions.

--- modules/myterminal/manifests/autosuggestions.pp
+++ modules/myterminal/manifests/autosuggestions.pp
@@ -1,6 +1,6 @@
 class myterminal::autosuggestions {
 	$zpautosuggestions = "/Users/${::boxen_user}/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
     repository { $zpautosuggestions:
-        source => 'tarruda/zsh-autosuggestions',
+        source => 'zsh-users/zsh-autosuggestions',
     }
 }
