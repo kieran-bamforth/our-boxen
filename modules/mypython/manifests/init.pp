f97f71d3845571b295ce1c8a48568c44a9a04eee
kieranbamforth <hello@kieranbamforth.me>
2016-05-21 22:55:18 +0100

    here is psutil.

--- modules/mypython/manifests/init.pp
+++ modules/mypython/manifests/init.pp
@@ -6,5 +6,10 @@ class mypython {
 		python => '2.7.6'
 	}
 
+	python::package { 'psutil for 2.7.6':
+		package => 'psutil',
+		python => '2.7.6'
+	}
+
 	class { 'python::global': version => '2.7.6' }
 }
