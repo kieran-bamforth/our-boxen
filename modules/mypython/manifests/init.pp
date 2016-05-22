9750bf47f952b4723235dff045788d5b9686d315
kieranbamforth <hello@kieranbamforth.me>
2016-05-22 01:12:30 +0100

    comments.

--- modules/mypython/manifests/init.pp
+++ modules/mypython/manifests/init.pp
@@ -6,6 +6,7 @@ class mypython {
 		python => '2.7.6'
 	}
 
+    # Having trouble with this? xcode-select install and see this link http://stackoverflow.com/questions/27976312/how-to-cope-with-non-gcc-compatible-code-in-os-x-yosemite-core-headers
 	python::package { 'psutil for 2.7.6':
 		package => 'psutil',
 		python => '2.7.6'
