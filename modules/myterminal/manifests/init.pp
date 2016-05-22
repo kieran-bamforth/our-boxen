3f0e67458b72c43cf2ff4a73b82240f524d81738
kieranbamforth <hello@kieranbamforth.me>
2016-05-22 11:02:26 +0100

    docker autocomplete.

--- modules/myterminal/manifests/init.pp
+++ modules/myterminal/manifests/init.pp
@@ -1,15 +1,16 @@
 class myterminal {
-	include iterm2::stable
-	include tmux
-	include zsh
+    include iterm2::stable
+    include tmux
+    include zsh
 
     require myterminal::pre
-    
+
     include myterminal::autojump
     include myterminal::autosuggestions
+    include myterminal::dockercompletion
     include myterminal::enhancd
     include myterminal::fzf
     include myterminal::powerline
-	include myterminal::tpm
+    include myterminal::tpm
     include myterminal::tree
 }
