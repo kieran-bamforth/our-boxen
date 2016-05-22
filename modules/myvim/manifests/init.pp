d2b012146d6f5d56eeaf20f916f3d739673dbf16
kieranbamforth <hello@kieranbamforth.me>
2016-05-22 11:31:26 +0100

    youcompleteme.

--- modules/myvim/manifests/init.pp
+++ modules/myvim/manifests/init.pp
@@ -1,21 +1,22 @@
 class myvim {
-	include macvim
-	vim::bundle { [
-		'altercation/vim-colors-solarized',
-		'ekalinin/Dockerfile.vim',
-        'elzr/vim-json',
-		'joonty/vdebug',
-		'kieran-bamforth/vim-phpunit',
-		'kshenoy/vim-signature',
-		'Lokaltog/vim-easymotion',
-		'rodjek/vim-puppet',
-        'scrooloose/nerdtree',
-		'scrooloose/syntastic',
-		'sjl/gundo.vim',
-		'tpope/vim-commentary',
-		'tpope/vim-dispatch',
-		'tpope/vim-fugitive',
-		'tpope/vim-sensible',
-		'tpope/vim-surround'
-	]: }
+  include macvim
+  vim::bundle { [
+    'altercation/vim-colors-solarized',
+    'ekalinin/Dockerfile.vim',
+    'elzr/vim-json',
+    'joonty/vdebug',
+    'kieran-bamforth/vim-phpunit',
+    'kshenoy/vim-signature',
+    'Lokaltog/vim-easymotion',
+    'rodjek/vim-puppet',
+    'scrooloose/nerdtree',
+    'scrooloose/syntastic',
+    'sjl/gundo.vim',
+    'tpope/vim-commentary',
+    'tpope/vim-dispatch',
+    'tpope/vim-fugitive',
+    'tpope/vim-sensible',
+    'tpope/vim-surround',
+    'Valloric/YouCompleteMe'
+  ]: }
 }
