930ba4e3e4356a5e05aeb52c4a457ddaa76841e0
kieranbamforth <hello@kieranbamforth.me>
2016-05-27 09:40:09 +0100

    remoed the bbpr.

--- modules/projects/manifests/bbpr.pp
+++
@@ -1,9 +0,0 @@
-# Project Manifests
-
-class projects::bbpr {
-	require projects::dotfiles
-	$repoName = "bbpr"
-	boxen::project { "${repoName}":
-		source => "git@bitbucket.org:kieranbamforth/${repoName}.git"
-	}
-}
