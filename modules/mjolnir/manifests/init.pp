78547ed41bc7a74a8d7ee4116579be491e4649b9
kieranbamforth <hello@kieranbamforth.me>
2016-09-18 17:04:01 +0100

    installing the app binary.

--- modules/mjolnir/manifests/init.pp
+++ modules/mjolnir/manifests/init.pp
@@ -1,4 +1,8 @@
 class mjolnir {
+  package { 'Mjolnir':
+    source   => 'https://github.com/sdegutis/mjolnir/releases/download/0.4.3/Mjolnir-0.4.3.zip',
+    provider => 'compressed_app'
+  }
   exec { 'luarocks install mjolnir.hotkey': }
   exec { 'luarocks install mjolnir.application': }
 }
