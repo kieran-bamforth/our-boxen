811ffc12b7b9b783a94bc3092cec20ff2a0efeaf
kieranbamforth <hello@kieranbamforth.me>
2016-11-22 10:28:11 +0000

    pip is broken.

--- modules/mypython/manifests/init.pp
+++ modules/mypython/manifests/init.pp
@@ -1,16 +1,21 @@
 class mypython {
 	python::version { '2.7.6': }
 
-	python::package { 'awscli for 2.7.6':
-		package => 'awscli',
-		python => '2.7.6'
-	}
+  # python::package { 'mycli for 2.7.6':
+  # 	package => 'mycli',
+  # 	python => '2.7.6'
+  # }
+
+  # python::package { 'awscli for 2.7.6':
+  # package => 'awscli',
+  # python => '2.7.6'
+  # }
 
     # Having trouble with this? xcode-select install and see this link http://stackoverflow.com/questions/27976312/how-to-cope-with-non-gcc-compatible-code-in-os-x-yosemite-core-headers
-	python::package { 'psutil for 2.7.6':
-		package => 'psutil',
-		python => '2.7.6'
-	}
+    # python::package { 'psutil for 2.7.6':
+    # package => 'psutil',
+    # python => '2.7.6'
+    # }
 
-	class { 'python::global': version => '2.7.6' }
+    # class { 'python::global': version => '2.7.6' }
 }
